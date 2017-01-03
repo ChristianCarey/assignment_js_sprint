61f847bf7146321c9f3953ad1bdb501a4eb1808f
Dexter Ford <dexter.ford.77@gmail.com>
2017-01-03 10:45:55 -0600

    Init

--- scripts.js
+++ scripts.js
@@ -1,21 +1,60 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(arr){ 
+    var max ; 
+    arr.forEach(function(el) {
+      if (max === undefined || el > max) {
+        max = el;
+      }
+    } );
+    return max;
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(str){  
+    var arr = str.split("");
+    var revArr = [];
+    arr.forEach(function(el) {
+      revArr.unshift(el);
+    } );
+    return revArr.join("");
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(str){
+    var arr = str.split("");
+    var output = [];
+    var nextCap = false;
+    arr.forEach(function(el) {
+      if (el === " " && nextCap === false) {
+        output.push("_");
+        nextCap = true;
+      }
+      else if (/^[a-zA-Z]+$/.test(el)) {
+        if (nextCap === true) {
+          output.push(el.toUpperCase());
+          nextCap = false;
+        }
+        else {
+          output.push(el);
+        }
+      }
+    } );
+    return output.join("");
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(arr1, arr2){ 
+     if (arr1.length !== arr2.length) {
+      return false;
+     }
+     else {
+      var i;
+      for(i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+     }
+     return true;
   },
 
   fizzBuzz: function(){  
